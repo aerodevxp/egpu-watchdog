9d8bc4d70d62ebc11b00fec9fc16deaf7301a658
aerodevxp <rnga3672@gmail.com>
2026-09-08 23:45:45 -0400

    settings igpu fix

--- homedir/bin/switch-internal.sh
+++ homedir/bin/switch-internal.sh
@@ -10,10 +10,7 @@ USER_HOME=$(getent passwd 1000 | cut -d: -f6)
 
 [ -z "$USER_NAME" ] && { echo "Could not find user with UID 1000"; exit 1; }
 
-sudo systemctl stop display-manager
-
 #SETTINGS (CHANGE THIS PATH TO YOUR DOCKSETTINGS LOCATION)
-sudo -i -u "$USER_NAME" /run/media/system/GAMES/docksettings/docksettings.sh -g igpu
 
 #HARDWARE
 #systemctl --user set-environment MESA_VK_DEVICE_SELECT=
@@ -25,6 +22,7 @@ if [ "$uptime_seconds" -lt 60 ]; then
     echo "System uptime is less than a minute ($uptime_seconds seconds)"
     exit
 else
+    sudo systemctl stop display-manager
     echo "System uptime is $uptime_seconds seconds"
     systemctl --machine="${USER_NAME}@.host" --user stop sunshine
     sudo pkill sunshine
@@ -35,6 +33,7 @@ else
     sleep 2
     sudo systemctl restart display-manager
     sleep 3
+    sudo -i -u "$USER_NAME" /run/media/system/GAMES/docksettings/docksettings.sh -g igpu
     echo 30000 | sudo tee /sys/class/backlight/*/brightness
     sleep 5
     INTERNAL_SINK=$(sudo -i -u "$USER_NAME" pactl list short sinks | grep analog-stereo | head -1 | cut -f2) [ -n "$INTERNAL_SINK" ]
